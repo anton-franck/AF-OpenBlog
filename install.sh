e3ba575446020f2d9afc25a28a9b78d994246262
Anton <anton.franck@icloud.com>
2025-07-05 01:16:02 +0200

    Fix Script

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ read -sp "Datenbank Root-Passwort: " DATABASE_ROOT_PASSWORD && echo
 read -sp "Datenbank Benutzer-Passwort: " STRAPI_DATABASE_PASSWORD && echo
 
 STRAPI_JWT_SECRET=$(openssl rand -base64 32)
+STRAPI_ADMIN_JWT_SECRET=$(openssl rand -base64 32)
 STRAPI_APP_KEYS=$(openssl rand -base64 64 | tr -d '\n')
 STRAPI_API_TOKEN_SALT=$(openssl rand -base64 32)
 STRAPI_TRANSFER_TOKEN_SALT=$(openssl rand -base64 32)
@@ -37,11 +38,12 @@ touch .env
 	grep -q "^# Strapi Keys" .env  || echo "\n# Strapi Keys" >> .env
 	grep -q "^STRAPI_JWT_SECRET=" .env  || echo "STRAPI_JWT_SECRET=\"$STRAPI_JWT_SECRET\"" >> .env
 	grep -q "^STRAPI_APP_KEYS=" .env  || echo "STRAPI_APP_KEYS=\"$STRAPI_APP_KEYS\"" >> .env
+    grep -q "^STRAPI_ADMIN_JWT_SECRET=" .env  || echo "STRAPI_ADMIN_JWT_SECRET=\"$STRAPI_ADMIN_JWT_SECRET\"" >> .env
 	grep -q "^STRAPI_API_TOKEN_SALT=" .env  || echo "STRAPI_API_TOKEN_SALT=\"$STRAPI_API_TOKEN_SALT\"" >> .env
 	grep -q "^STRAPI_TRANSFER_TOKEN_SALT=" .env  || echo "STRAPI_TRANSFER_TOKEN_SALT=\"$STRAPI_TRANSFER_TOKEN_SALT\"" >> .env
 	grep -q "^# Frontend Configuration" .env  || echo "\n# Frontend Configuration" >> .env
 	grep -q "^FRONTEND_PORT=" .env  || echo "FRONTEND_PORT=80:3000" >> .env
-	grep -q "^STRAPI_URL=" .env  || echo "FRONTEND_STRAPI_URL=\"http://strapi:1337\"" >> .env
+	grep -q "^STRAPI_URL=" .env  || echo "STRAPI_URL=\"http://strapi:1337\"" >> .env
     grep -q "^STRAPI_API_KEY=" .env  || echo "STRAPI_API_KEY=\"\"" >> .env
 
 make update
